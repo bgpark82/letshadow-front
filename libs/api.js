2177e5a7511ceda0190c6bd8444a43c845e1d1f7
bgpark <bgpark82@gmail.com>
2021-01-18 01:08:14 +0900

    feat: add google login

---
+++ libs/api.js
@@ -0,0 +1,99 @@
+export async function fetchVideos(token) {
+    const videos = await fetch(
+        `https://www.googleapis.com/youtube/v3/videos?part=snippet,player,contentDetails,status,topicDetails,id&myRating=like&access_token=${token}`,
+        {
+            method: "get",
+            headers: {
+                Authorization: "Bearer " + token,
+            },
+        }
+    );
+    return await videos.json();
+}
+
+export async function fetchGoogleEmail(token) {
+    try {
+        const response = await fetch(
+            `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${token}`
+        );
+        return await response.json();
+    } catch (e) {
+        throw new Error("token expired!");
+    }
+}
+
+export async function fetchServerToken() {
+    let params = new URLSearchParams();
+    params.append("grant_type", "password");
+    params.append("scope", "read write");
+    params.append("username", "pop8682@gmail.com");
+    params.append("password", "123");
+
+    const accessTokenRes = await fetch(`http://localhost:8080/oauth/token`, {
+        method: "POST",
+        headers: {
+            Authorization: `Basic ${window.btoa("test:test")}`,
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        credentials: "same-origin",
+        body: params,
+    });
+
+    return await accessTokenRes.json();
+}
+
+export async function fetchGoogleToken(code) {
+    let params = new URLSearchParams();
+    params.append("code", code);
+    params.append(
+        "client_id",
+        "758204078687-dhoc57phmqfj5epv6vvi327kguumm9p8.apps.googleusercontent.com"
+    );
+    params.append("client_secret", "vRDY1-vBeRsXstnZlrYqrgGF");
+    params.append("grant_type", "authorization_code");
+    params.append("redirect_uri", "http://localhost:5500");
+
+    const response = await fetch(`https://oauth2.googleapis.com/token`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: params,
+    });
+    return await response.json();
+}
+
+export function fetchGoogleCode() {
+    // Google's OAuth 2.0 endpoint for requesting an access token
+    var oauth2Endpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+
+    // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+    var form = document.createElement("form");
+    form.setAttribute("method", "GET"); // Send as a GET request.
+    form.setAttribute("action", oauth2Endpoint);
+
+    // Parameters to pass to OAuth 2.0 endpoint.
+    var params = {
+        client_id:
+            "758204078687-dhoc57phmqfj5epv6vvi327kguumm9p8.apps.googleusercontent.com",
+        redirect_uri: "http://localhost:5500",
+        response_type: "code",
+        scope: "profile openid email https://www.googleapis.com/auth/youtube",
+        include_granted_scopes: "true",
+        state: "pass-through value",
+        access_type: "offline",
+    };
+
+    // Add form parameters as hidden input values.
+    for (var p in params) {
+        var input = document.createElement("input");
+        input.setAttribute("type", "hidden");
+        input.setAttribute("name", p);
+        input.setAttribute("value", params[p]);
+        form.appendChild(input);
+    }
+
+    // Add form to page and submit it to open the OAuth 2.0 endpoint.
+    document.body.appendChild(form);
+    form.submit();
+}
--- libs/utils.js
+++ libs/utils.js
@@ -1,10 +1,14 @@
 export const getTimestamp = (timestamp) => {
     if (!timestamp) return "00:00";
-    
+
     const min = Math.floor(timestamp / 60);
     const sec = Math.floor(timestamp % 60);
     const minStr = min < 10 ? "0" + min : min;
     const secStr = sec < 10 ? "0" + sec : sec;
     console.log("timestamp: ", timestamp, min, sec);
     return minStr + ":" + secStr;
+};
+
+export const log = (message, ...data) => {
+    console.log(`[ INFO ] ${message} : `, ...data);
 };
