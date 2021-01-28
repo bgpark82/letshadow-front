babb257ad8cd55f4c22c3c7861ce2c68da1ed83a
bgpark <bgpark82@gmail.com>
2021-01-29 00:18:37 +0900

    feat: add video size style

--- pages/index.js
+++ pages/index.js
@@ -24,6 +24,7 @@ new (function () {
         this.player = YL("player", {
             videoId: this.videoId,
             width: "100%",
+            height: "100%",
             playerVars: {
                 playsinline: 1,
             },
