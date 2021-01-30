9509f50a0f0d681503babfc5669f00264f61344c
bgpark <bgpark82@gmail.com>
2021-01-31 00:22:46 +0900

    fix: change server uri

--- libs/api.js
+++ libs/api.js
@@ -1,5 +1,5 @@
-const BASE_SERVER_URI = "http://localhost:8080";
-// const BASE_SERVER_URI = "https://letshadow.me";
+// const BASE_SERVER_URI = "http://localhost:8080";
+const BASE_SERVER_URI = "https://letshadow.me";
 const REDIRECT_URI = BASE_SERVER_URI + "/oauth/callback";
 const CLIENT_ID =
     "758204078687-dhoc57phmqfj5epv6vvi327kguumm9p8.apps.googleusercontent.com";
