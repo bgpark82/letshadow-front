d5e53bca9a928d7d728f0015685be572c1547e35
bgpark <bgpark82@gmail.com>
2021-02-12 11:50:44 +0900

    LF-5 ESLint 설정

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: ['airbnb-typescript'],
+    parserOptions: {
+        project: './tsconfig.json'
+    },
+    rules: {
+        '@typescript-eslint/object-curly-spacing':0,
+        '@typescript-eslint/space-infix-ops':0,
+    }
+};
